291baa4748a72819120780f55d219c0bb75bfd4e
NE7K <sjjang16@naver.com>
2025-04-08 10:02:49 +0900

    Fixed subject code

--- 1/2020U2324_5w.c
+++ 1/2020U2324_5w.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 1000  // 배열 최대 크기
+#define MAX_SIZE 10  // 읽을 최대 정수 개수를 10개로 제한
 
 int main() {
     FILE *file;
     int data[MAX_SIZE];
     int count = 0;
-    int max;
+    int max1, max2;
 
     file = fopen("inputData.txt", "r");
     if (file == NULL) {
@@ -15,7 +15,7 @@ int main() {
         return 1;
     }
 
-    // 파일에서 정수를 읽어 배열에 저장
+    // 최대 10개 정수를 파일에서 읽음
     while (fscanf(file, "%d", &data[count]) == 1 && count < MAX_SIZE) {
         count++;
     }
@@ -25,19 +25,35 @@ int main() {
     if (count == 0) {
         printf("파일에 데이터가 없습니다.\n");
         return 1;
+    } else if (count == 1) {
+        printf("입력된 정수 개수: 1\n");
+        printf("최대값: %d\n", data[0]);
+        printf("두 번째로 큰 값이 없습니다.\n");
+        return 0;
     }
 
-    // 최대값 찾기
-    max = data[0];
-    for (int i = 1; i < count; i++) {
-        if (data[i] > max) {
-            max = data[i];
+    // 최대값과 두 번째 최대값 찾기
+    if (data[0] > data[1]) {
+        max1 = data[0];
+        max2 = data[1];
+    } else {
+        max1 = data[1];
+        max2 = data[0];
+    }
+
+    for (int i = 2; i < count; i++) {
+        if (data[i] > max1) {
+            max2 = max1;
+            max1 = data[i];
+        } else if (data[i] > max2) {
+            max2 = data[i];
         }
     }
 
     // 결과 출력
     printf("입력된 정수 개수: %d\n", count);
-    printf("최대값: %d\n", max);
+    printf("최대값: %d\n", max1);
+    printf("두 번째로 큰 값: %d\n", max2);
 
     return 0;
 }
